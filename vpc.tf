255e27e4183960f18900c0f428b66d9840b61385
Paulino02 <tpaulino12000@gmail.com>
2024-06-10 23:38:28 -0300

    commit

--- vpc.tf
+++ vpc.tf
@@ -16,16 +16,6 @@ resource "aws_subnet" "PublicSubnet" {
   }
 }
 
-# 3 : create a private subnet
-resource "aws_subnet" "PrivSubnet" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "PrivateSubnet"
-  }
-}
-
 # 4 : create IGW
 resource "aws_internet_gateway" "myIgw" {
   vpc_id = aws_vpc.myvpc.id
@@ -52,57 +42,12 @@ resource "aws_route_table_association" "PublicRTAssociation" {
   route_table_id = aws_route_table.PublicRT.id
 }
 
-# 7: Create Elastic IP for NAT Gateway
-resource "aws_eip" "nat_eip" {
-  vpc = true
+resource "aws_instance" "teste" {
+  ami           = "ami-04b70fa74e45c3917"     # Substitua pelo ID da AMI desejada
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.PublicSubnet.id  # Substitua pelo ID da sub-rede desejada
+
   tags = {
-    Name = "NAT EIP"
+    Name = "MinhaInstanciaEC2"
   }
-}
-
-# 8: Create NAT Gateway
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.PublicSubnet.id
-  tags = {
-    Name = "NAT Gateway"
-  }
-}
-
-# 9: Route Table for private subnet
-resource "aws_route_table" "PrivateRT" {
-  vpc_id = aws_vpc.myvpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
-  }
-  tags = {
-    Name = "PrivateRT"
-  }
-}
-
-# 10: Route table association for private subnet
-resource "aws_route_table_association" "PrivateRTAssociation" {
-  subnet_id      = aws_subnet.PrivSubnet.id
-  route_table_id = aws_route_table.PrivateRT.id
-}
-
-# Output VPC ID
-output "vpc_id" {
-  value = aws_vpc.myvpc.id
-}
-
-# Output Public Subnet ID
-output "public_subnet_id" {
-  value = aws_subnet.PublicSubnet.id
-}
-
-# Output Private Subnet ID
-output "private_subnet_id" {
-  value = aws_subnet.PrivSubnet.id
-}
-
-# Output NAT Gateway ID
-output "nat_gateway_id" {
-  value = aws_nat_gateway.nat_gw.id
 }
