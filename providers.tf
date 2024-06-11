292060d92725602c0e3b9355f96024a66c746e22
Paulino02 <tpaulino12000@gmail.com>
2024-06-10 22:38:06 -0300

    commit

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.53.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.0"
+  region = "us-east-1"
 }
